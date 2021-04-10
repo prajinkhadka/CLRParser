3802ee15cba040e31f8378aff11ed41a0f419e62
Prajin Khadka <34759925+prajinkhadka@users.noreply.github.com>
2021-04-10 22:28:29 +0545

    Refactor

--- firstfollow.py
+++ firstfollow.py
@@ -19,7 +19,6 @@ class Terminal:
     def __str__(self):
         return self.symbol
 
-# ------------------------------------------------------------------
 
 class NonTerminal:
 
@@ -37,11 +36,10 @@ class NonTerminal:
 
 # ------------------------------------------------------------------
 
-def compute_first(symbol): #chr(1013) corresponds (ϵ) in Unicode
+def compute_first(symbol): 
 
     global production_list, nt_list, t_list
 
-# if X is a terminal then first(X) = X
     if symbol in t_list:
         return set(symbol)
 
@@ -50,7 +48,6 @@ def compute_first(symbol): #chr(1013) corresponds (ϵ) in Unicode
         
         if head!=symbol: continue
 
-# if X -> is a production, then first(X) = epsilon
         if body=='':
             nt_list[symbol].add_first(chr(1013))
             continue
@@ -58,24 +55,19 @@ def compute_first(symbol): #chr(1013) corresponds (ϵ) in Unicode
         
 
         for i, Y in enumerate(body):
-# for X -> Y1 Y2 ... Yn, first(X) = non-epsilon symbols in first(Y1)
-# if first(Y1) contains epsilon, 
-#   first(X) = non-epsilon symbols in first(Y2)
-#   if first(Y2) contains epsilon
-#   ...
+
             if body[i]==symbol: continue
             t=compute_first(Y)
             nt_list[symbol].add_first(t-set(chr(1013)))
             if chr(1013) not in t:
                 break 
-# for i=1 to n, if Yi contains epsilon, then first(X)=epsilon
             if i==len(body)-1: 
                 nt_list[symbol].add_first(chr(1013))
     return nt_list[symbol].first
 
 # ------------------------------------------------------------------
 
-def get_first(symbol): #wrapper method for compute_first
+def get_first(symbol): 
 
     return compute_first(symbol)
 
@@ -85,8 +77,7 @@ def compute_follow(symbol):
 
     global production_list, nt_list, t_list
 
-# if A is the start symbol, follow (A) = $
-    if symbol == list(nt_list.keys())[0]: #this is okay since I'm using an OrderedDict
+    if symbol == list(nt_list.keys())[0]: 
         nt_list[symbol].add_follow('$')
 
     for prod in production_list:    
@@ -95,15 +86,13 @@ def compute_follow(symbol):
         for i, B in enumerate(body):        
             if B != symbol: continue
 
-# for A -> aBb, follow(B) = non-epsilon symbols in first(b)
             if i != len(body)-1:
                 nt_list[symbol].add_follow(get_first(body[i+1]) - set(chr(1013)))
 
-# if A -> aBb where first(b) contains epsilon, or A -> aB then follow(B) = follow (A)
             if i == len(body)-1 or chr(1013) in get_first(body[i+1]) and B != head: 
                 nt_list[symbol].add_follow(get_follow(head))
 
-# ------------------------------------------------------------------
+
 
 def get_follow(symbol):
 
@@ -142,7 +131,6 @@ def main(pl=None):
                 if head not in nt_list.keys():
                     nt_list[head]=NonTerminal(head)
 
-                #for all terminals in the body of the production
                 for i in body:
                     if not 65<=ord(i)<=90:
                         if i not in t_list.keys(): t_list[i]=Terminal(i)
@@ -151,22 +139,7 @@ def main(pl=None):
                     
                 ctr+=1
 
-        '''if pl!=None:
-            for i, prod in enumerate(pl):
-                if prod.lower() in ['end', '']:
-                    del pl[i:]
-                    break
-                head, body=prod.split('->')
-                if head not in nt_list.keys():
-                    nt_list[head]=NonTerminal(head)
-                #for all terminals in the body of the production
-                for i in finditer(t_regex, body):
-                    s=i.group()
-                    if s not in t_list.keys(): t_list[s]=Terminal(s)
-                #for all non-terminals in the body of the production
-                for i in finditer(nt_regex, body):
-                    s=i.group()
-                    if s not in nt_list.keys(): nt_list[s]=NonTerminal(s)'''
+        
         print("prod list eta", production_list)
         print("pl eta", pl)       
         return user_gram
